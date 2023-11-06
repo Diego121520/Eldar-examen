8e0dcd47cb5d8b2fbf43e96cbdb1d5e7f8faf6cf
diego121520 <otazo.diego20@gmail.com>
2023-11-06 06:33:55 -0300

    data.sql

---
+++ backend/src/main/resources/static/sql/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO guest_table (first_name, last_name, dni, birth_date, confirmed)
+VALUES
+    ('John', 'Doe', '123456789', '1990-01-15', false),
+    ('Jane', 'Smith', '987654321', '1985-05-20', false),
+    ('Alice', 'Johnson', '456789123', '1992-08-10', false),
+    ('Bob', 'Williams', '789123456', '1980-11-25', false),
+    ('Eva', 'Davis', '654321987', '1994-03-08', false),
+    ('Michael', 'Brown', '321987654', '1977-09-30', false),
+    ('Sara', 'Taylor', '234567891', '1998-12-05', false),
+    ('William', 'Wilson', '567891234', '1991-06-14', false),
+    ('Laura', 'Martinez', '112233445', '1987-04-02', false),
+    ('Daniel', 'Garcia', '554433221', '1983-07-18', false);
